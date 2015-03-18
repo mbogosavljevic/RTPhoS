dccb1ee137284e0f08adde192c4b632b130151c2
mbogosavljevic <mbogosavljevic@aob.rs>
2015-03-18 11:47:49 +0100

    more robust path handling and some comments added

--- seekfits.py
+++ seekfits.py
@@ -18,9 +18,12 @@ import numpy as np
 def seekfits(path_to_watch, tsleep):
 
   before = dict ([(f, None) for f in os.listdir (path_to_watch)])
+
+  # counter needed just to print out progress
   count = 0.
+
   try:
-     while 1:
+    while 1:
          count = count + 1
          print int(count), "*LISTENING*", path_to_watch, time.strftime('%X %x %Z')
          after = dict ([(f, None) for f in os.listdir (path_to_watch)])
@@ -30,11 +33,11 @@ def seekfits(path_to_watch, tsleep):
            print "Added: ", ", ".join (added)
            for filein in enumerate(added):                  
              # check if it is a fits file
-             filename=filein[1]
-             # WARNING - hardcoded the '.fits' and '.fit' extensions
+             filename = path_to_watch + filein[1]          
+             # WARNING - hardcoded the '.fits' or '.fit' extensions
              if (filename.endswith('.fits') or filename.endswith('.fit')):
                # Can load both data and header with this trick
-               data, hdr=pyfits.getdata(filename, header=True)              
+               data, hdr = pyfits.getdata(filename, header=True)              
                # Get Observation Header for image and display it
                obsdate = hdr['DATE-OBS']
                print obsdate
@@ -43,13 +46,19 @@ def seekfits(path_to_watch, tsleep):
            
          before = after
          time.sleep (tsleep)   # Wait for tsleep seconds before repeating
-
   except KeyboardInterrupt:
-     pass
+    pass
 
 if __name__ == "__main__":
 
     import sys
+
+    # check if path to watch ends with '/'
     path_to_watch = sys.argv[1]
+    if not(path_to_watch.endswith('/')):
+      path_to_watch = path_to_watch + '/'
+
+    # this is the time period in seconds between checkin the path directory
     tsleep  = float(sys.argv[2])
+
     seekfits(path_to_watch, tsleep)
