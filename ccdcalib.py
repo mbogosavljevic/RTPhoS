49e83f7ee606c53961549e90aec9ccabb700b6c4
zioannou <zach.ioannou@gmail.com>
2015-05-06 16:05:41 +0400

    Bias & Dark Subtraction Work

--- ccdcalib.py
+++ ccdcalib.py
@@ -16,14 +16,18 @@ def makebias(dataref):
 	# Search for all bias files in the current directory
 	# Warning: will search for all files with the text 'bias' in their filename.
 	biasfiles = sorted(glob.glob('*bias*'))
-	print "Found ",len(biasfiles), "bias frames"
+	biasnum = len(biasfiles)	
+	print "Found ", biasnum, "bias frames"
+	if biasnum==0:
+		masterbias=0.0
+		print "Warning: No bias frames found data will not be bias calibrated!"
+		return masterbias
 
 	# Determine the size of the image based on the size of the first bias file.
 	nx = pyfits.getval(biasfiles[0], 'NAXIS1')
 	ny = pyfits.getval(biasfiles[0], 'NAXIS2')
 
 	# Set up the array that will hold all the bias images.
-	biasnum = len(biasfiles)
 	bias_images = np.ndarray((biasnum,nx,ny),dtype=float)
 
 	# Go round this loop and fill the bias images array.
@@ -33,7 +37,7 @@ def makebias(dataref):
 
 	# Median combine the bias frame
 	masterbias = np.median(bias_images, axis=0)
-	print masterbias.mean()
+	print "Bias Median: ", np.median(masterbias)
 
 	# Output the masterbias frame.
 	# Filename hardwired as masterbias.fits
@@ -45,10 +49,60 @@ def makebias(dataref):
 
 	return masterbias
 
+# Create a Masterdark frame from available bias frames.
+#-------------------------------------------------------------
+def makedark(dataref):
+	# Initialize darkframes dictionary
+	darkframes = {}
+    
+	# Search for all dark files in the current directory
+	# Warning: will search for all files with the text 'dark' in their filename.
+	darkfiles = sorted(glob.glob('*dark*'))
+	darknum = len(darkfiles)		
+	print "Found ",darknum, "dark frames"
+	if darknum==0:
+		masterdark=0.0
+		print "WARNING: No dark frames found data will not be bias calibrated!"
+		return masterdark
 
-# load the image which is displayed in ds9 (from disk!)
+	# Determine the size of the image based on the size of the first dark file.
+	nx = pyfits.getval(darkfiles[0], 'NAXIS1')
+	ny = pyfits.getval(darkfiles[0], 'NAXIS2')
+
+	# Set up the array that will hold all the dark images.
+	dark_images = np.ndarray((darknum,nx,ny),dtype=float)
+
+	# Go round this loop and fill the bias images array.
+	for i in range(0,darknum):
+		print 'Reading file',i
+		dark_images[i] = pyfits.getdata(darkfiles[i])
+
+	# Median combine the dark frame
+	masterdark = np.median(dark_images, axis=0)
+
+	# Check to see if a bias exists and if it does subract it from the dark.
+	if os.path.isfile('masterbias.fits'):	
+		biasimg = pyfits.open('masterbias.fits')
+		masterbias = biasimg[0].data
+		masterdark = masterdark - masterbias
+	# If the bias does not exist, create it.
+	else:
+		masterbias = makebias(dataref)
+		masterdark = masterdark - masterbias
+
+	print "Dark Median: ", np.median(masterdark)
+	# Output the masterdark frame.
+	# Filename hardwired as masterdark.fits
+	hdu=pyfits.PrimaryHDU(masterdark)
+	hdu.writeto('masterdark.fits')
+  
+	# Free the memory!  
+	del dark_images
+
+	return masterdark
+
 #ref_filename = win.get("file")
-ref_filename = 'test1753.fits'
+ref_filename = 'J1753.fits'
 dataref, hdr = pyfits.getdata(ref_filename, header=True)     
 print("... Working ...")
 
@@ -61,7 +115,7 @@ biascor = darkcor = flatcor = False  # set calib flags to False
 # been calibrated.
 if "ZEROCOR" in keywordlist:
 	biascor = True
-if "DARKCOR" in keywordlist:
+if "zach" in keywordlist:
 	darkcor = True
 if "FLATCOR" in keywordlist:
 	flatcor = True
@@ -83,13 +137,20 @@ if not biascor:
 		dataref = dataref - masterbias
 		print "Bias calibration performed"
 
-"""
 if not darkcor:
-	darkimg = pyfits.open('masterdark.fits')
-	darkref = darkimg[0].data
-	dataref = dataref - darkref
-	print "Uncalibrated image: Removed Dark"
+	print "Frame is not Dark calibrated"
+	print "Proceeding with removing the dark..."
+	if os.path.isfile('masterdark.fits'):	
+		darkimg = pyfits.open('masterdark.fits')
+		masterdark = darkimg[0].data
+		dataref = dataref - masterdark
+		print "Dark calibration performed"
+	else:
+		masterdark = makedark(dataref)
+		dataref = dataref - masterdark
+		print "Dark calibration performed"
 
+"""
 if not flatcor:
 	flatimg = pyfits.open('masterflat.fits')
 	flatref = flatimg[0].data
@@ -98,7 +159,7 @@ if not flatcor:
 """
 # Write calibrated file to disk
 hdu=pyfits.PrimaryHDU(dataref)
-hdu.writeto('testbias.fits')
+hdu.writeto('testcalib.fits')
 print "All Done!"
 
 
@@ -106,37 +167,6 @@ print "All Done!"
 """
 # Create a Masterdark frame from available dark frames.
 -------------------------------------------------------------
-# Initialize darkframes dictionary
-darkframes = {}
-    
-# Search for all dark files in the current directory
-# Warning: will search for all files with the text 'dark' in their filename.
-darkfiles = sorted(glob.glob('*dark*'))
-print "Found ",len(darkfiles), "dark frames"
-
-# Determine the size of the image based on the size of the first dark file.
-nx = pyfits.getval(darkfiles[0], 'NAXIS1')
-ny = pyfits.getval(darkfiles[0], 'NAXIS2')
-
-# Set up the array that will hold all the dark images.
-darknum = len(darkfiles)
-dark_images = np.ndarray((darknum,nx,ny),dtype=float)
-
-# Go round this loop and fill the bias images array.
-for i in range(0,darknum):
-    print 'Reading file',i
-    dark_images[i] = pyfits.getdata(darkfiles[i])
-
-# Median combine the dark frame
-masterdark = np.median(dark_images, axis=0)
-
-# Output the masterdark frame.
-# Filename hardwired as masterdark.fits
-hdu=pyfits.PrimaryHDU(masterdark)
-hdu.writeto('masterdark.fits')
-  
-# Free the memory!  
-del dark_images
 
 # Create a Masterflat frame from available flat field frames.
 -------------------------------------------------------------
