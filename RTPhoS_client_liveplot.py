ac758398a357bebff913491d74e85eac40f559e5
Milan <mbogosavljevic@aob.rs>
2016-06-13 12:51:52 +0400

    fixed time axis bug and error removal-replot

--- RTPhoS_client_liveplot.py
+++ RTPhoS_client_liveplot.py
@@ -117,7 +117,7 @@ yrawcomperr=[]   # Raw comparison error bars
 ydflux=[]        # Differential photometry counts
 ydfluxerr=[]     # Differential photometry error bars
 yseeing=[]       # Seeing data
-
+UTCtimes = []
 #################################
 # if using live updating log file
 ################################
@@ -129,39 +129,44 @@ if uselog:
         # open the log file
         # read all data that is currently there
         nline = 0
+        # nline has value 0 for line 1 in file
         with open(args.logfile,'r') as fp:
           for line in fp:
-              nline = nline + 1
               line = line.strip()  
               columns = line.split()
+              nline = nline + 1
               # skip comment line
               if columns[0] == '#RTPhoS:':
-                  print ("skipped:", columns)
+                  print "skipped:", columns
               else:
                   obsid         = columns[0]
                   serverport    = columns[1]
                   UTCdatetime   = columns[2]
                   bandpass      = columns[3]
-                  BJD           = float(columns[4]) 
-                  targetflux    = float(columns[5])
-                  targetfluxerr = float(columns[6])
-                  compflux      = float(columns[7])
-                  compfluxerr   = float(columns[8])
-                  seeing        = float(columns[9])
+                  BJD           = np.float64(columns[4]) 
+                  targetflux    = np.float32(columns[5])
+                  targetfluxerr = np.float32(columns[6])
+                  compflux      = np.float32(columns[7])
+                  compfluxerr   = np.float32(columns[8])
+                  seeing        = np.float16(columns[9])
 
                   # TBD:
                   # use just data that matches -filter argument
                   # something like matches = [x for x in a if x=='a']
+                  # UTCtimes is a list, stores all actual times for
+                  # recaluclating minutes_before_now later
+                  UTCtimes.append(UTCdatetime)
                   newx = minutes_before_now(UTCdatetime)
-                  xdata = np.append(xdata,newx)
-                  yrawtarget=np.append(yrawtarget,targetflux)
-                  yrawtargeterr=np.append(yrawtargeterr,targetfluxerr)
-                  yrawcomp=np.append(yrawcomp,compflux)
-                  yrawcomperr=np.append(yrawcomperr,compfluxerr)
+                  print UTCdatetime, round(newx,3)
+                  xdata.append(round(newx,3))
+                  yrawtarget.append(targetflux)
+                  yrawtargeterr.append(targetfluxerr)
+                  yrawcomp.append(compflux)
+                  yrawcomperr.append(compfluxerr)
                   # for now use the same raw counts for diff flux:
                   ydflux=yrawtarget
                   ydfluxerr=yrawtargeterr
-                  yseeing=np.append(yseeing,seeing)
+                  yseeing.append(seeing)
 
         # remember the time when file has been read last
         lastdatafiletime = os.stat(args.logfile).st_mtime
@@ -199,43 +204,75 @@ if uselog:
         line3a, = ax3.plot(xdata, yrawtarget, 'ro')
         line3b, = ax3.plot(xdata, yrawcomp, 'go') 
         fig.legend([line3a, line3b,], ['Target', 'Comp'], loc='upper left', \
-                   bbox_to_anchor=[0.9,0.68], shadow=True, numpoints=1, prop={'size':8})
+                bbox_to_anchor=[0.9,0.68], shadow=True, numpoints=1, prop={'size':8})
+        # plot the error bars
+        errorlines3a = []
+        errorlines3b = []
+        for k in range(0,len(xdata)):
+            l, = ax3.plot([xdata[k],xdata[k]],[yrawtarget[k]-yrawtargeterr[k],\
+                                          yrawtarget[k]+yrawtargeterr[k]], 'r-')
+            errorlines3a.append(l)
+            l, = ax3.plot([xdata[k],xdata[k]],[yrawcomp[k]-yrawcomperr[k],\
+                                          yrawcomp[k]+yrawcomperr[k]], 'r-')
+            errorlines3b.append(l)
 
         # Differential Photometry
         ax4.grid(True)
         ax4.margins(0.1,0.1)
         ax4.set_ylabel('Relative Flux')
-        line4, = ax4.plot(xdata, yrawtarget, 'bo')
+        line4, = ax4.plot(xdata, ydflux, 'bo')
         fig.legend([line4,], ['Diff'], loc='upper left', \
                    bbox_to_anchor=[0.9,0.48], shadow=True, numpoints=1, prop={'size':8})
+        # plot the error bars and collect them in
+        # a new variable
+        errorlines4 = []
+        for k in range(0,len(xdata)):
+            l, = ax4.plot([xdata[k],xdata[k]],[ydflux[k]-ydfluxerr[k],\
+                                          ydflux[k]+ydfluxerr[k]], 'r-')
+            errorlines4.append(l)
 
         # Seeing 
         ax5.grid(True)
         ax5.margins(0.1,0.1)
-        ax5.set_xlabel('Time JD+ ') #+str(stripjd))
+        if args.bjd:
+            ax5.set_xlabel('Time JD+ ') 
+        else:
+            ax5.set_xlabel('Minutes [now = 0]')
+ 
         ax5.set_ylabel('Pixels')
         line5, = ax5.plot(xdata, yseeing, 'bo') 
         fig.legend([line5,], ['Seeing'], loc='upper left', \
                    bbox_to_anchor=[0.9,0.27], shadow=True, numpoints=1, prop={'size':8})
+
+ 
+        npoints_current = len(xdata)
+        fig.canvas.draw()
 ######### while
         while True:
                # check if input data file has been modified
                datafiletime = os.stat(args.logfile).st_mtime
 ################
+               
                if datafiletime != lastdatafiletime:
                    # update time of last file update
                    lastdatafiletime = datafiletime
                    # check the new length of the file 
                    filelen = file_len(args.logfile)
                    print "File has now: ", filelen
+                   # recalculate xdata for old points
 
+                   xdata = []
+                   for t in UTCtimes:
+                       newtime = minutes_before_now(t)
+                       xdata.append(round(newtime,3)) 
+                   
 ####################
-                   print ("Starting to read from line:", nline)
-                   if nline <= filelen:
+                   print "Starting to read from line:", nline+1
+                   if (nline+1) <= filelen:
                      for row in range(nline,filelen):
                          # move to the line stated by row variable
                          with open(args.logfile,'r') as fp:
-                            j = 1
+                            j = 0
                             for line in fp:
                              if j == row:
                               useline = line
@@ -253,15 +290,17 @@ if uselog:
                          # TBD:
                          # here we shoud make a check if the data is
                          # still in the same bandpass!
-                         newBJD           = float(useline[4]) 
-                         newtargetflux    = float(useline[5])
-                         newtargetfluxerr = float(useline[6])
-                         newcompflux      = float(useline[7])
-                         newcompfluxerr   = float(useline[8])
-                         newseeing        = float(useline[9])
-                         print newUTCdatetime
-                         newx = minutes_before_now(newUTCdatetime) 
-                         xdata = np.append(xdata,newx)
+                         newBJD           = np.float64(useline[4]) 
+                         newtargetflux    = np.float32(useline[5])
+                         newtargetfluxerr = np.float32(useline[6])
+                         newcompflux      = np.float32(useline[7])
+                         newcompfluxerr   = np.float32(useline[8])
+                         newseeing        = np.float16(useline[9])
+
+                         UTCtimes.append(newUTCdatetime)
+
+                         newx = minutes_before_now(newUTCdatetime)
+                         xdata = np.append(xdata,round(newx,3))
                          yrawtarget=np.append(yrawtarget,newtargetflux)
                          yrawtargeterr=np.append(yrawtargeterr,newtargetfluxerr)
                          yrawcomp=np.append(yrawcomp,newcompflux)
@@ -281,6 +320,8 @@ if uselog:
                          # cropmax = np.amax(target_crop)
                          ## Attempt for a reasonable intensity scale 
                          # maxintens = ((cropmax-cropmin)/2.0)+cropmin
+
+                         # clear the plot
                          
                          ## Target thumbnail
                          # ax1.plot([14,14],[0,14],'r:')             # Plot cross-hairs
@@ -293,25 +334,56 @@ if uselog:
                          # ax2.imshow(comp_crop, cmap='gray', norm=LogNorm(vmin=cropmin, vmax=maxintens))
                          
                          # Target Raw counts
+                         #print len(xdata)
+                         #rint len(yrawtarget)
+                         #line3a, = ax3.plot(xdata, yrawtarget, 'ro')
+                         #line3b, = ax3.plot(xdata, yrawcomp, 'go') 
+                         
                          line3a.set_xdata(xdata)
                          line3a.set_ydata(yrawtarget)
                          line3b.set_xdata(xdata)
                          line3b.set_ydata(yrawcomp)
-                         ax3.plot([newx,newx],[newtargetflux-newtargetfluxerr, newtargetflux+newtargetfluxerr], 'r-')
-                         ax3.plot([newx,newx],[newcompflux-newcompfluxerr, newcompflux+newcompfluxerr], 'g-')       
                          ax3.relim()
                          
+                         safe_lines3a = errorlines3a
+                         safe_lines3b = errorlines3b
+                         errorlines3a = []
+                         errorlines3b = []
+                         # remove previous error bars
+                         for k in range(0,npoints_current):
+                             ax3.lines.remove(safe_lines3a[k])
+                             ax3.lines.remove(safe_lines3b[k])
+                         for k in range(0,len(xdata)):
+                             l, = ax3.plot([xdata[k],xdata[k]], \
+                                           [yrawtarget[k]-yrawtargeterr[k],\
+                                            yrawtarget[k]+yrawtargeterr[k]], 'r-')
+                             errorlines3a.append(l)
+                             l, = ax3.plot([xdata[k],xdata[k]], \
+                                           [yrawcomp[k]-yrawcomperr[k],\
+                                            yrawcomp[k]+yrawcomperr[k]], 'r-')
+                             errorlines3b.append(l)
+
                          # Differential flux
                          line4.set_xdata(xdata)
                          line4.set_ydata(yrawtarget)
-                         ax4.plot([newx,newx],[newtargetflux-newtargetfluxerr, newtargetflux+newtargetfluxerr], 'b-')
                          ax4.relim()
-                         
+                         # plot the error bars
+                         safe_lines4 = errorlines4
+                         errorlines4 = []
+                         for k in range(0,npoints_current):
+                             ax4.lines.remove(safe_lines4[k])
+                         for k in range(0,len(xdata)):
+                             l, = ax4.plot([xdata[k],xdata[k]],\
+                                           [ydflux[k]-ydfluxerr[k],\
+                                            ydflux[k]+ydfluxerr[k]], 'r-')
+                             errorlines4.append(l)
+
                          # Seeing plot
                          line5.set_xdata(xdata)
                          line5.set_ydata(yseeing)
                          ax5.relim()
-                         
+
+                         npoints_current = len(xdata)
                          fig.canvas.draw()
 ######################### end for rows
 #################### end if nline <= filelen
